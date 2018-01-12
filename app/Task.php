f477266a64763ca93a5188cb5962d4c160bb809b
Gaetano Carpinato <kejan73@gmail.com>
2018-01-12 14:39:20 +0100

    creazione Task

---
+++ app/Task.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'date_expiration',
+        'id_board',
+        'id_user'
+    ];
+}
---
+++ database/migrations/2018_01_12_122154_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->default('No Title');
+            $table->string('description');
+            $table->integer('id_board')->unsigned();
+            $table->integer('id_user')->unsigned()->nullable();
+            $table->date('date_expiration')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
